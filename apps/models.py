a143f63e50eddfc28e0f0bb47ebc86a3574acfcf
deepanshu-jain1999 <deepanshuj1999@gmail.com>
2018-03-24 00:17:31 +0530

    Add abstract based inheritance

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    """
+    Abstract based inheritance
+    here this model alone have no signifance
+    but with other model it will include these field into that model
+    so here name will include in Account class
+    """
+    name = models.CharField(max_length=100, primary_key=True)
+
+    class Meta:
+        abstract = True
+
+
+class Account(Student):
+    gmail = models.CharField(max_length=300)
+    github = models.CharField(max_length=300)
+

fef69804712bdbd938db9de604b34dd6df657b49
deepanshu-jain1999 <deepanshuj1999@gmail.com>
2018-03-24 01:17:47 +0530

    Multiple inheritance

--- apps/admin.py
+++ apps/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from apps.models import Account
+from apps.models import Account, Place, Restaurant
+
 
 admin.site.register(Account)
+admin.site.register(Place)
+admin.site.register(Restaurant)
+
--- apps/models.py
+++ apps/models.py
@@ -15,8 +15,9 @@ class Student(models.Model):
     """
     name = models.CharField(max_length=100,
                             primary_key=True,
-                            related_name="%(app_label)s_%(class)s_related",
-                            related_query_name="%(app_label)s_%(class)s",)
+                            # related_name="%(app_label)s_%(class)s_related",
+                            # related_query_name="%(app_label)s_%(class)s",
+                           )
 
     class Meta:
         abstract = True
@@ -26,3 +27,22 @@ class Account(Student):
     gmail = models.CharField(max_length=300)
     github = models.CharField(max_length=300)
 
+
+# move to multiple inheritance
+
+
+class Place(models.Model):
+    """
+    In multiple inheritance
+    1. Place have only own field while Restaurant have both class field
+    2. If we create a restaurant model then place already will made but reverse not true
+    3.
+
+    """
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
+
+
+class Restaurant(Place):
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
